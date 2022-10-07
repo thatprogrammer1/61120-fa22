91a18c502912461139594358b99251c5ad07c400
Luka Govedic <luka.govedic@gmail.com>
2022-10-07 04:16:10 -0400

    Updated run-tests script

--- tests/a2/run-tests.sh
+++ tests/a2/run-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-INTERPRETER=$1 # don't forget a ./ preceeding your interp e.g. ./mitscript
+# USAGE: ./run-tests.sh [public|private] <path to mitscript>
+TESTS=$1
+INTERPRETER=$2 # don't forget a ./ preceeding your interp e.g. ./mitscript
 TIMEOUT=60
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -7,37 +9,22 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TOTAL=0
 COUNT=0
 
-for filename in $SCRIPT_DIR/public/bad*.mit; do
-    echo $filename
-    OUT=$(timeout $TIMEOUT $INTERPRETER $filename 2>&1)
-    CODE=$?
-    if [[ $CODE -ne 0 ]]; then
-        COUNT=$((COUNT+1))
-    else
-        echo "Fail: $(basename $filename) (exit code $CODE) (OUT: $OUT)"
-    fi
-    TOTAL=$((TOTAL+1))
-done
-
-for filename in $SCRIPT_DIR/public/good*.mit; do
-    echo $filename
-    timeout $TIMEOUT $INTERPRETER $filename > tmp.out
-    CODE=$?
-    if diff tmp.out $SCRIPT_DIR/public/$(basename $filename).out; then
-        COUNT=$((COUNT+1))
-    else
-        echo "Fail: $(basename $filename) (exit code $CODE)"
-    fi
-    TOTAL=$((TOTAL+1))
-    rm -f tmp.out
-done
-
-echo "Passed $COUNT out of $TOTAL public tests"
-
 PTOTAL=0
 PCOUNT=0
 
-for filename in $SCRIPT_DIR/private/*.mit; do
+for filename in $SCRIPT_DIR/$TESTS/*.mit; do
+    if [[ $filename == *bad*.mit ]]; then
+        echo $filename
+        OUT=$(timeout $TIMEOUT $INTERPRETER $filename 2>&1)
+        CODE=$?
+        if [[ $CODE -ne 0 ]]; then
+            COUNT=$((COUNT+1))
+        else
+            echo "Fail: $(basename $filename) (exit code $CODE) (OUT: $OUT)"
+        fi
+        TOTAL=$((TOTAL+1))
+        continue
+    fi
     echo $filename
     if test -f $filename.in; then
         timeout $TIMEOUT $INTERPRETER $filename < $filename.in > tmp1.out 2> tmp1.err
@@ -45,7 +32,7 @@ for filename in $SCRIPT_DIR/private/*.mit; do
         timeout $TIMEOUT $INTERPRETER $filename > tmp1.out 2> tmp1.err
     fi
     CODE=$?
-    if [[ $(cat $SCRIPT_DIR/private/$(basename $filename).out) =~ [A-Z][A-Za-z]+Exception ]]; then
+    if [[ $(cat $SCRIPT_DIR/$TESTS/$(basename $filename).out) =~ [A-Z][A-Za-z]+Exception ]]; then
         if [[ $(cat tmp1.err tmp1.out) = *"${BASH_REMATCH[0]}"* ]]; then
             PCOUNT=$((PCOUNT+1))
             COUNT=$((COUNT+1))
@@ -54,7 +41,7 @@ for filename in $SCRIPT_DIR/private/*.mit; do
             echo "Fail Exception: $(basename $filename) (exit code $CODE)"
         fi
     else
-        if diff tmp1.out $SCRIPT_DIR/private/$(basename $filename).out; then
+        if diff tmp1.out $SCRIPT_DIR/$TESTS/$(basename $filename).out; then
             PCOUNT=$((PCOUNT+1))
             COUNT=$((COUNT+1))
         else
@@ -67,5 +54,4 @@ for filename in $SCRIPT_DIR/private/*.mit; do
     rm -f tmp1.out tmp1.err
 done
 
-echo "Passed $PCOUNT out of $PTOTAL private tests"
 echo "Passed $COUNT out of $TOTAL tests"
